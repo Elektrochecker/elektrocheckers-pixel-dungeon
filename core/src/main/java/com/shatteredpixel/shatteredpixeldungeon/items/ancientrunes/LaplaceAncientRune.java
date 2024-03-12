c48ea2475b51ea2e22e6cb9b1c26a258ede1a28f
Timon Lilje <timon.lilje@gmail.com>
2024-03-12 15:51:33 +0100

    laplace ancient rune

---
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/ancientrunes/LaplaceAncientRune.java
@@ -0,0 +1,79 @@
+/*
+ * Pixel Dungeon
+ * Copyright (C) 2012-2015 Oleg Dolya
+ *
+ * Shattered Pixel Dungeon
+ * Copyright (C) 2014-2024 Evan Debenham
+ * 
+ * Elektrocheckers Pixel Dungeon
+ * Copyright (C) 2023-2024 Timon Lilje
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+package com.shatteredpixel.shatteredpixeldungeon.items.ancientrunes;
+
+import com.shatteredpixel.shatteredpixeldungeon.effects.Speck;
+import com.shatteredpixel.shatteredpixeldungeon.items.Item;
+import com.shatteredpixel.shatteredpixeldungeon.items.potions.Potion;
+import com.shatteredpixel.shatteredpixeldungeon.items.potions.PotionOfStrength;
+import com.shatteredpixel.shatteredpixeldungeon.items.potions.exotic.ExoticPotion;
+import com.shatteredpixel.shatteredpixeldungeon.items.scrolls.Scroll;
+import com.shatteredpixel.shatteredpixeldungeon.items.scrolls.ScrollOfTransmutation;
+import com.shatteredpixel.shatteredpixeldungeon.items.scrolls.ScrollOfUpgrade;
+import com.shatteredpixel.shatteredpixeldungeon.items.scrolls.exotic.ExoticScroll;
+import com.shatteredpixel.shatteredpixeldungeon.sprites.ItemSpriteSheet;
+import com.watabou.utils.Reflection;
+
+public class LaplaceAncientRune extends AncientRuneInventory {
+	
+	{
+		image = ItemSpriteSheet.ANCIENTRUNE_LAPLACE;
+	}
+
+	protected boolean usableOnItem(Item item) {
+		return (item instanceof Potion || item instanceof Scroll) && !(item instanceof ExoticPotion || item instanceof ExoticScroll);
+	}
+	
+	@Override
+	protected void onItemSelected(Item item) {
+		item.detach(curUser.belongings.backpack);
+		Item result;
+
+		if (item instanceof Scroll) {
+			result = Reflection.newInstance(ExoticScroll.regToExo.get(item.getClass()));
+		} else if (item instanceof Potion) {
+			result = Reflection.newInstance(ExoticPotion.regToExo.get(item.getClass()));
+		} else {
+			//refund target
+			item.collect();
+			return;
+		}
+
+		
+		Reflection.newInstance(result.getClass()).collect();
+		Reflection.newInstance(result.getClass()).collect();
+		
+		if (!(item instanceof PotionOfStrength || item instanceof ScrollOfUpgrade || item instanceof ScrollOfTransmutation)) {
+			Reflection.newInstance(result.getClass()).collect();
+		}
+
+		curUser.sprite.emitter().start(Speck.factory(Speck.ENCHANT2), 0.4f, 4);
+	}
+	
+	@Override
+	public int value() {
+		return 250;
+	}
+}
